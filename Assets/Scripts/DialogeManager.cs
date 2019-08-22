d721960e06d2994d3be102dc9bc4127cfb3c6f0c
Antonio Diaz <antonio.diaz.02071998@gmail.com>
2019-08-22 17:19:32 -0500

    Dialoge Interaction system is here

---
+++ Assets/Scripts/DialogeManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+// place on the Dialogue Panel that has all the Dialog UI. Make sure it has a CanvasGroup attached to it
+// Takes the dialoge from a character and prints it out in the dialog box. Also places the name of the character and its image in the right spots
+
+public class DialogeManager : MonoBehaviour
+{
+    public Image speakerImage;                              // image of where the character will go
+    public Text dialogueText;                               // the text of where the dialog will be written
+    public Text nameText;                                   // the text of the name
+
+    public AudioClip[] typingClips;
+
+    private Queue<string> sentences;
+    bool isDialoging = false;
+    CanvasGroup canvasGroup;
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (isDialoging) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                DisplayNextSentence();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Gets the Dialogue and sprite. Enqueues all the dialogue and places the image.
+    /// </summary>
+    /// <param name="dialogue"></param>
+    /// <param name="imgage"></param>
+    public void StartDialogue(Dialogue dialogue, Sprite imgage) { 
+        canvasGroup.alpha = 1f;
+        speakerImage.sprite = imgage;
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sent in dialogue.sentences) {
+            sentences.Enqueue(sent);
+
+            
+        }
+        Debug.Log(sentences.Count);
+        DisplayNextSentence();
+
+        isDialoging = true;
+    }
+
+    /// <summary>
+    /// When pressed the next sentence in the dialoge will be shown
+    /// </summary>
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence) {
+        int choice = Random.Range(0, typingClips.Length - 1);
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray()) {
+            source.PlayOneShot(typingClips[choice]);
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingClips[choice].length);
+        }
+    }
+
+    /// <summary>
+    /// Turns off the the canvas
+    /// </summary>
+    void EndDialogue() {
+        Debug.Log("End of convo");
+        canvasGroup.alpha = 0f;
+        isDialoging = false;
+    }
+}
---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Place on the Pluck
+// Gets the info of who Pluck is talking to and sets the button to press so you can talk to said npc.
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public bool canInteract = false;                                // whether you can talk to the npc or not
+
+    UIController uiController;
+    NPCInteraction npcInInteraction = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiController = FindObjectOfType<UIController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canInteract)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                npcInInteraction.SendDialogeInfo();
+                uiController.SetSpeakingInteractionButton(false);
+                canInteract = false;
+                // make sure to freeze movement while Dialoging with other npcs
+            }
+        }
+    }
+
+    /// <summary>
+    /// sets if you can talk and sets the info of the npc character you are talking to
+    /// </summary>
+    /// <param name="interact"></param>
+    /// <param name="npc"></param>
+    public void SetInteract(bool interact, NPCInteraction npc)
+    {
+        canInteract = interact;
+        npcInInteraction = npc;
+    }
+}
