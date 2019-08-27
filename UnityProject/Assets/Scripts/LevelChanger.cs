7a993c28a319fb62a5b9976d7931189386b6e3a2
Antonio Diaz <antonio.diaz.02071998@gmail.com>
2019-08-27 14:29:36 -0500

    Scen transitioning

--- UnityProject/Assets/Scripts/LevelChanger.cs
+++ UnityProject/Assets/Scripts/LevelChanger.cs
@@ -1,26 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelChanger : MonoBehaviour
 {
     public string nextLevelName;
+    public string currentLevelName;
 
-    Animator anim;
+    ScreenFader fader;
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        fader = GetComponent<ScreenFader>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1)) {
+            NextLevel();
+        }
     }
 
 
-    public void ChangeLevel() {
-       // anim.
+    public void NextLevel() {
+        fader.Fade("out");
+        StartCoroutine(LoadScene());
+
+    }
+
+    IEnumerator LoadScene() {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(nextLevelName);
     }
 }
