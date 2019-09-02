345ba5f61bd4bfb4e4fe01f3fed8295c53a6c005
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-09-02 18:01:18 -0500

    Crumbling and Moving Platforms

---
+++ UnityProject/Assets/Scripts/DrawPathGizmo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class visualizes in the editor the waypoints and path of a moving platform.
+public class DrawPathGizmo : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Vector3 lastWayPoint = Vector3.zero;
+        Vector3 FirstWayPoint = Vector3.zero;
+        foreach (Transform waypoint in transform)
+        {
+            Gizmos.DrawSphere(waypoint.position, .3f);
+
+            if (lastWayPoint != Vector3.zero)
+            {
+                Gizmos.DrawLine(lastWayPoint, waypoint.position);
+            }
+            else
+            {
+                FirstWayPoint = waypoint.position;
+            }
+            lastWayPoint = waypoint.position;
+        }
+        Gizmos.DrawLine(lastWayPoint, FirstWayPoint);
+    }
+}
---
+++ UnityProject/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script move a platform from one waypoint to the next.
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] DrawPathGizmo patrolPath;
+    [SerializeField] float waypointTolerance = 2f;
+    [SerializeField] float timeToWaitAtWaypoints = .5f;
+    [SerializeField] float speed = .5f;
+
+    private int nextPathPointIndex = 0;
+    private Vector3 nextPathPointPos;
+    private float timeAtWaypoint = 0;
+
+    void Awake()
+    {
+        nextPathPointPos = patrolPath.transform.GetChild(nextPathPointIndex).position;
+    }
+
+    void Update()
+    {
+        if (timeAtWaypoint + timeToWaitAtWaypoints <= Time.time)
+        {
+            // Move the position a step closer to the target.
+            float step = speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, nextPathPointPos, step);
+
+            if (Vector3.Distance(nextPathPointPos, transform.position) < waypointTolerance)
+            {
+                nextPathPointIndex = (nextPathPointIndex + 1) % patrolPath.transform.childCount;
+                nextPathPointPos = patrolPath.transform.GetChild(nextPathPointIndex).position;
+                timeAtWaypoint = Time.time;
+            }
+        }
+    }
+}
