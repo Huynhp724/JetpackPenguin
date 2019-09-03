c4f9f1e31703d2219ca9319341fba94bb7e978fa
Antonio Diaz <antonio.diaz.02071998@gmail.com>
2019-09-03 11:45:27 -0500

    Auto stash before merge of "Programming" and "origin/Programming"

---
+++ UnityProject/Assets/Scripts/BaseSealFSM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseSealFSM : StateMachineBehaviour
+{
+    public GameObject npc;
+    public GameObject player;
+
+    public float speed = 3.5f;
+    public float rotationSpeed = 2.5f;
+
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        npc = animator.gameObject;
+
+        player = npc.GetComponentInChildren<Seal>().GetPlayer();
+    }
+}
