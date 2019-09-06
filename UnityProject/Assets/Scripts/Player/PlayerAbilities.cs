bf5054e0a41709ea70b04fe21b0442cc36965111
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-09-06 15:11:08 -0500

    Changed the icebomb throw physics and made more customization

--- UnityProject/Assets/Scripts/Player/PlayerAbilities.cs
+++ UnityProject/Assets/Scripts/Player/PlayerAbilities.cs
@@ -10,25 +10,35 @@ public class PlayerAbilities : MonoBehaviour
     [SerializeField] Transform throwStartPoint;
     [SerializeField] GameObject snowBomb;
     [SerializeField] GameObject playerModel;
+    [Tooltip("Time player has to wait between a throw. In real seconds.")]
     [SerializeField] float timeBetweenThrows = .5f;
-    [SerializeField] float minThrowVelocity = 5f;
-    [SerializeField] float MaxThrowVelocity = 10f;
-    [SerializeField] float throwAngle = 45f;
+    [Tooltip("Angle in degrees. decreasing will increase the MAX distance thrown.")]
+    [Range(.1f, 90)]
+    [SerializeField] float minThrowAngle = 35f;
+    [Tooltip("Angle in degrees. Increasing will increase the MIN distance thrown.")]
+    [Range(10, 90)]
+    [SerializeField] float maxThrowAngle = 60f;
+    [Tooltip("How high the peak of a throw will be.")]
+    [SerializeField] float arcHeight = 10f;
+    [Tooltip("How fast the player can adjust the throw by holding the adjust button.")]
     [SerializeField] float adjustThrowRate = .1f;
-    [SerializeField] float snowbombGravityMultiplier = 2f;
+    [Tooltip("How fast the projectile will go through the arc.")]
+    [SerializeField] float projectileSpeedMultiplier = 2f; //This is actually a gravity multiplier, but for the sake of the game designers they can adjust this for speed. 
 
     private float throwVelocity;
+    private float throwAngle = 45f;
     private float timeOfLastThrow = 0f;
     private Player player;
     private LaunchArcRenderer lineRenderer;
     private PlayerController playerController;
+    private float xVelocity;
+    private float yVelocity;
 
     private void Awake()
     {
         player = ReInput.players.GetPlayer(0);
         lineRenderer = throwStartPoint.GetComponent<LaunchArcRenderer>();
         playerController = GetComponent<PlayerController>();
-        throwVelocity = minThrowVelocity;
     }
 
     void Update()
@@ -37,23 +47,27 @@ public class PlayerAbilities : MonoBehaviour
         {
             AimSnowBomb();
         }
-        else //if(player.GetButtonUp("Aim Bomb"))
+        else
         {
             lineRenderer.UnrenderArc();
-            throwVelocity = minThrowVelocity;
+            throwAngle = maxThrowAngle;
         }
     }
 
     
-    // This function allows the player to aim a snowbomb throw. By holding the adjust throw button the arc will increase till max velocity. Pressing the throw bomb button will throw the bomb.
+    // This function allows the player to aim a snowbomb throw. By holding the adjust throw button the arc will increase till max angle. Pressing the throw bomb button will throw the bomb.
     private void AimSnowBomb()
-    {
+    {       
         if (player.GetButton("Adjust Throw"))
         {
             AdjustAim();
         }
 
-        lineRenderer.RenderArc(throwVelocity, throwAngle, snowbombGravityMultiplier * Mathf.Abs(Physics.gravity.y));
+        // Calculate the velocity based off the desired max height and the speed (gravity) of the projectile.
+        float radianAngle = Mathf.Deg2Rad * throwAngle;
+        throwVelocity = Mathf.Sqrt((2 * arcHeight * Math.Abs(Physics.gravity.y * projectileSpeedMultiplier)) / (Mathf.Sin(radianAngle) * Mathf.Sin(radianAngle)));
+
+        lineRenderer.RenderArc(throwVelocity, throwAngle, Mathf.Abs(Physics.gravity.y * projectileSpeedMultiplier));
 
         if (player.GetButtonDown("Throw Bomb") && timeOfLastThrow + timeBetweenThrows <= Time.time)
         {
@@ -61,10 +75,10 @@ public class PlayerAbilities : MonoBehaviour
         }
     }
 
-    // While called this method increases the velocity of the throw until reached the max velocity.
+    // While called this method decreases the angle of the throw until reachs the min angle. This will increase the distance travelled.
     private void AdjustAim()
     {
-        throwVelocity = Mathf.MoveTowards(throwVelocity, MaxThrowVelocity, adjustThrowRate);
+        throwAngle = Mathf.MoveTowards(throwAngle, minThrowAngle, adjustThrowRate);
     }
     
     // Instantiates a snow bomb with the same velcoity of the projected arc.
@@ -75,7 +89,7 @@ public class PlayerAbilities : MonoBehaviour
         float radianAngle = Mathf.Deg2Rad * throwAngle;
         float xVelocity = Mathf.Cos(radianAngle) * throwVelocity;
         float yVelocity = Mathf.Sin(radianAngle) * throwVelocity;
-        currentBomb.GetComponent<SnowBomb>().setGravityMultiplier(snowbombGravityMultiplier);
+        currentBomb.GetComponent<SnowBomb>().setGravityMultiplier(projectileSpeedMultiplier);
         currentBomb.GetComponent<Rigidbody>().velocity = throwStartPoint.transform.TransformDirection(xVelocity, yVelocity, 0);
     }
 }
