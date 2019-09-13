54e9795bbc445f65ca6bf8539e5fdbe8f7a38363
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-09-12 20:35:36 -0500

    Breakable wall prefab

--- UnityProject/Assets/Scripts/BreakableWall.cs
+++ UnityProject/Assets/Scripts/BreakableWall.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script checks if the player slides into the gameobject fast enough, if so then it is detroyed. 
 public class BreakableWall : MonoBehaviour
 {
     [SerializeField] float playerSpeedThreshhold = 5f;
@@ -11,10 +12,7 @@ public class BreakableWall : MonoBehaviour
         PlayerController playerControl = other.gameObject.GetComponent<PlayerController>();
         if (playerControl)
         {
-            //float angle = Mathf.Atan2(playerControl.controller.velocity.z, playerControl.controller.velocity.x);
-            //float playerSpeed = Mathf.Sqrt(Mathf.Pow(playerControl.controller.velocity.x, 2) + Mathf.Pow(playerControl.controller.velocity.z, 2));
             float playerSpeed = playerControl.getCurrentSlideSpeed();
-            print(playerSpeed);
             if (playerSpeed > playerSpeedThreshhold)
             {
                 Destroy(gameObject);
