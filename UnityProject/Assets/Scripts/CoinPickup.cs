6a115f59ad11b055415912e9c42a4ed3a3b9a3fe
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-09-22 15:37:16 -0500

    Collectibles have life

--- UnityProject/Assets/Scripts/CoinPickup.cs
+++ UnityProject/Assets/Scripts/CoinPickup.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] float rotateSpeed = 1f;
+    [SerializeField] float bobDegree = .75f;
+
+    private float intialY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        intialY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up, rotateSpeed);
+        transform.position = new Vector3(transform.position.x, intialY + (Mathf.Sin(Time.time) * bobDegree), transform.position.z);
     }
 
     private void OnTriggerEnter(Collider other)
