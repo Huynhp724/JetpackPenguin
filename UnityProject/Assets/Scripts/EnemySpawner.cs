51cc25f5e198e08b63300eab8b0f8a04a74549df
Antonio Diaz <antonio.diaz.02071998@gmail.com>
2019-09-24 15:44:07 -0500

    Started Enemy Spawner

---
+++ UnityProject/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyToSpawn;
+    public Transform spawnPosition;
+    public PathCreator pathToFollow;
+
+    Transform startPositionOfPath;
+
+    public void Spawn() {
+        GameObject go = Instantiate(enemyToSpawn, spawnPosition);
+        PathFollower path = go.GetComponent<PathFollower>();
+        path.followPath = false;
+        path.pathCreator = pathToFollow;
+
+        startPositionOfPath = pathToFollow.gameObject.transform.GetChild(0);
+
+        // make Ai set to go to path
+
+    }
+}
