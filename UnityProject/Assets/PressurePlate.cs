724338292a95e410a84fcb62d281baa2a265e505
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-09-26 13:14:11 -0500

    Door and Pressure Plate Prefab

--- UnityProject/Assets/PressurePlate.cs
+++ UnityProject/Assets/PressurePlate.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject door;
+    [SerializeField] int iceBlockLayer;
+
     void Start()
     {
         
@@ -14,5 +16,17 @@ public class PressurePlate : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == iceBlockLayer)
+        {
+            collision.transform.position = transform.position + (Vector3.up);
+            collision.transform.rotation = transform.rotation;
+            collision.gameObject.layer = 0; //For now, later may have player able to pick it up again.
+            collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            Destroy(door);
+        }
     }
 }
