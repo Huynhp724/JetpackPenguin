7d6946b8668be929ec98aeae4a1a898c668e6992
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-10-23 15:52:59 -0500

    Basic cutscenes can be made in editor without animation

---
+++ UnityProject/Assets/Scripts/Camera/CutScene.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CutScene : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] CutSceneCamera[] cameras;
+    [SerializeField] CinemachineBrain cmBrain;
+    private PlayerController pc;
+    private PlayerAbilities pa;
+
+    private int OFF = 5;
+    private int ON = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pc = player.GetComponent<PlayerController>();
+        pa = player.GetComponent<PlayerAbilities>();
+        pc.enabled = false;
+        pa.enabled = false;
+        StartCoroutine(NextCamera(0));
+    }
+
+    IEnumerator NextCamera(int index)
+    {
+        print("Camera: " + index);
+        //cmBrain.m_DefaultBlend.m_Time = 5;
+        while(cmBrain.IsBlending)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForSecondsRealtime(cameras[index].timeToWait);
+
+        cameras[index].setPriority(OFF);
+
+        if (index + 1 < cameras.Length)
+        {
+            cameras[index + 1].setPriority(ON);
+            StartCoroutine(NextCamera(index + 1));
+        }
+        else
+        {
+            pc.enabled = true;
+            pa.enabled = true;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProject/Assets/Scripts/Camera/CutSceneCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CutSceneCamera : MonoBehaviour
+{
+    CinemachineVirtualCamera vc;
+    public float timeToWait = 1;
+
+    private void Start()
+    {
+        vc = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void setPriority(int x)
+    {
+        vc.Priority = x;
+    }
+}
