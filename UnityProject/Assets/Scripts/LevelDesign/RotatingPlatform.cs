023e0cf13bf4b2732467ff96295a3d7201d88380
Mikey <mab160530@utdallas.edu>
2019-10-27 03:13:08 -0500

    Finished first white box for level three, fixed a few various collision issues

---
+++ UnityProject/Assets/Scripts/LevelDesign/RotatingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPlatform : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float xAngle, yAngle, zAngle;
+
+    //public Rigidbody rb;
+    //public Vector3 rotationVelocity;
+
+    private void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+
+        //rotationVelocity = new Vector3(0, 0, 1);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(xAngle, yAngle, zAngle, Space.World);
+
+        //Quaternion deltaRotation = Quaternion.Euler(rotationVelocity * Time.deltaTime);
+        //rb.MoveRotation(rb.rotation * deltaRotation);
+
+    }
+}
