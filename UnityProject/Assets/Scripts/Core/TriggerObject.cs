cc7a07740ed28c028c26ce8c1288c94d38e4833d
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-11-26 16:30:20 -0600

    Optimized Village Scene
    Broke the scene into chunks and turns them on and off depending where the player is at.

---
+++ UnityProject/Assets/Scripts/Core/TriggerObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerObject : MonoBehaviour
+{
+    [SerializeField] GameObject objectToTrigger;
+    [SerializeField] bool disableObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponentInParent<PlayerController>())
+        {
+            if(disableObject)
+            {
+                objectToTrigger.SetActive(false);
+            }
+            else
+            {
+                objectToTrigger.SetActive(true);
+            }
+        }
+    }
+}
