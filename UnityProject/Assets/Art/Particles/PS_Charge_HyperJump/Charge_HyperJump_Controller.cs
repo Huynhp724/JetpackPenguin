f6e51186ee208c38c90c850c1a2c7f4dbc9f19c2
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-11-26 12:49:37 -0600

    Added more particle effects in

---
+++ UnityProject/Assets/Art/Particles/PS_Charge_HyperJump/Charge_HyperJump_Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charge_HyperJump_Controller : MonoBehaviour
+{
+    [Header("StartJetPackRechargeParticles();")]
+    [Header("StopJetPackRechargeParticles();")]
+    [Header("Public Functions:")]
+    [Space]
+    [SerializeField] private ParticleSystem rechargePS;
+    [SerializeField] private bool isDebugControls = false;
+    private bool isPlaying = false;
+
+    void Update()
+    {
+        Debugger();
+    }
+
+    void Debugger()
+    {
+        if (isDebugControls)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && !isPlaying)
+            {
+                StartJetPackRechargeParticles();
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StopJetPackRechargeParticles();
+            }
+        }
+    }
+
+    public void StartJetPackRechargeParticles()
+    {
+        rechargePS.Play();
+        isPlaying = true;
+    }
+
+    public void StopJetPackRechargeParticles()
+    {
+        rechargePS.Stop();
+        isPlaying = false;
+    }
+}
