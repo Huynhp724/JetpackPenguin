aec27099fba79b65d0d75e4f679c2fc05ab69e15
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-12-01 21:34:21 -0600

    Added win screen and Globe shatter effect

--- UnityProject/Assets/Scripts/LevelDesign/DomeWall.cs
+++ UnityProject/Assets/Scripts/LevelDesign/DomeWall.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class DomeWall : MonoBehaviour
 {
+    [SerializeField] PlayerWinScreen winScreen;
+    [SerializeField] float delayTillWin = 1f;
+    [SerializeField] GameObject globeBreakParticles;
+
+    private void Start()
+    {
+        if (winScreen == null)
+            winScreen = FindObjectOfType<PlayerWinScreen>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         PlayerController pc = collision.gameObject.GetComponentInParent<PlayerController>();
         if(pc && (pc.getChargedJumped()))
         {
+            winScreen.openWinScreen(delayTillWin);
+            Instantiate(globeBreakParticles, transform.parent.position, transform.rotation);
             GetComponentInParent<Dome>().destroyDome();
         }
     }
---
+++ UnityProject/Assets/Scripts/UI/PlayerWinScreen.cs
@@ -0,0 +1,51 @@
+﻿using Rewired;
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerWinScreen : MonoBehaviour
+{
+    [SerializeField] Animator winMenu;
+
+    private PlayerController playerController;
+    private ScreenManager sm;
+    private Pause pauseController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = GetComponent<ScreenManager>();
+        sm.ClosePanelBasic(winMenu.gameObject);
+        playerController = FindObjectOfType<PlayerController>();
+        pauseController = GetComponent<Pause>();
+    }
+
+
+    public void openWinScreen(float delayTime)
+    {
+        StartCoroutine(openScreen(delayTime));
+    }
+
+    IEnumerator openScreen(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        sm.OpenPanelBasic(winMenu.gameObject);
+        playerController.enabled = false;
+        playerController.GetComponent<Rigidbody>().isKinematic = true;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        pauseController.cantPause = true;
+    }
+
+    public void closeWinScreen()
+    {
+        sm.ClosePanelBasic(winMenu.gameObject);
+        playerController.enabled = true;
+        playerController.GetComponent<Rigidbody>().isKinematic = false;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        pauseController.cantPause = false;
+    }
+}
