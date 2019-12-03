db7d1e1677927f9a6f50e7fca79ff6023596fb41
Antonio Diaz <antonio.diaz.02071998@gmail.com>
2019-12-03 02:41:56 -0600

    Fixed death scene and Gate animations

---
+++ UnityProject/Assets/Scripts/TestScripts/GateAnimationTransition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateAnimationTransition : MonoBehaviour
+{
+
+    bool firstTime = true;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firstTime) {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("FirstGateClose")) {
+                anim.SetBool("FirstTime", false);
+                firstTime = false;
+            }
+        }
+    }
+}
